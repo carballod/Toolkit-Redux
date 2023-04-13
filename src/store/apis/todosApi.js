8488b9de65b13386fac3d765b980c2584a02ee4e
Daniela Carballo <danscarballo@gmail.com>
2023-04-13 16:47:45 -0300

    Consumir el API mediante el custom hook

--- src/store/apis/todosApi.js
+++ src/store/apis/todosApi.js
@@ -1,18 +1,20 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const todosApi = createApi({
-    reducerPath: 'todosApi',
+    reducerPath: 'todos',
 
     baseQuery: fetchBaseQuery({
         baseUrl: 'https://jsonplaceholder.typicode.com',
     }),
 
     endpoints: (builder) => ({
+
         getTodos: builder.query({
             query: () => '/todos',
         }),
+
     }),
 
 });
 
-export const {  } = todosApi;
+export const { useGetTodosQuery } = todosApi;
